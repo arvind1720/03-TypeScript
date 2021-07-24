74a2fddf3b179a21c9b8d1e653269ba672eb78f6
Arvind Ellangopan <arvindellan1@gmail.com>
2021-07-25 05:01:18 +0800

    last

---
+++ 04-Server/build/routes/loginRoutes.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
+var router = express_1.Router();
+exports.router = router;
---
+++ 04-Server/src/controllers/decorators/Methods.ts
@@ -0,0 +1,10 @@
+ 
+
+export enum Methods{
+
+    get='get',
+    post='post',
+    patch='patch',
+    del='delete',
+    put='put'
+}
